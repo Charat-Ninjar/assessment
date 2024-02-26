bff9b8af9d1a3fc37d9dc120be4b3e7d2aa018f1
Charat-Ninjar <charatvc@gmail.com>
2024-02-26 23:25:36 +0700

    refactoring code

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exeption/ApiExceptionHandler.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exeption/ApiExceptionHandler.java
@@ -10,16 +10,6 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(value = {BadRequestException.class})
-	public ResponseEntity<Object> handleBadRequestException(BadRequestException badRequestException){
-
-		ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
-				badRequestException.getMessage(),
-				HttpStatus.BAD_REQUEST
-		);
-		return new ResponseEntity<>(apiExceptionResponse, HttpStatus.BAD_REQUEST);
-	}
-
 	@ExceptionHandler(value = {NotFoundException.class})
 	public ResponseEntity<Object> handleNotFoundException(NotFoundException notFoundException){
 		ApiExceptionResponse apiExceptionResponse = new ApiExceptionResponse(
